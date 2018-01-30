4604b5fe16ccba5d659c400ce0233ea6c2740933
ryanhancock1 <ryanhancock1@mail.weber.edu>
2018-01-30 10:14:41 -0700

    initial

---
+++ module4/multibranch.c
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  multibranch.c
+ *
+ *    Description:  Nested Branches
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:10:39 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ryan Hancock (), ryanhancock1@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000;
+    if(income)
+    {
+        printf("income is %d\n", income);
+    }
+
+    else
+    {
+        printf("No income\n");
+    }
+
+
+    return 0;
+}
+// Function Definitions
+
+
