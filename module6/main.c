86b1eb2ffb63530291043bf1fccfd6117ae21247
ryanhancock1 <ryanhancock1@mail.weber.edu>
2018-02-22 01:25:02 -0700

    initial commit

--- module6/main.c
+++ module6/main.c
@@ -16,8 +16,9 @@
  * =====================================================================================
  */
 #include <stdio.h>
-
+#include <string.h>
 // Constants
+const int PLAY = 5;
 
 // Function Prototypes
 void display(int jersey[], int rating[], int n);
@@ -25,22 +26,20 @@ void display(int jersey[], int rating[], int n);
 int main()
 {
     char choice = ' ';
-    int jersey[5];
-    int rating[5];
-    int j=0,r=0;
+    int jersey[PLAY];
+    int rating[PLAY];
+    int num, new;
 
-    for(int i = 0; i<5; i++)
+    for(int i = 0; i<PLAY; i++)
     {
         printf("Enter player %d's jersey number:\n", i+1);
-        scanf("%d", &j);
-        jersey[i] = j;
+        scanf("%d", &jersey[i]);
         printf("Enter player %d's rating:\n", i+1);
-        scanf("%d", &r);
-        rating[i]=r;
+        scanf("%d", &rating[i]);
 		printf("\n");
     }
 printf("ROSTER\n");
-for(int i = 0; i<5; i++)
+for(int i = 0; i<PLAY; i++)
 {
 	printf("Player %d -- Jersey number: %d, Rating: %d\n", i+1, jersey[i], rating[i]);
 }
@@ -61,63 +60,66 @@ printf("MENU\n");
 		switch(choice)
 		{
 			case 'u':
-				printf("Enter jersey number:\n");
-				scanf("%d", &j);
-				printf("Enter a new rating for a player:\n");
-				scanf("%d", &r);
+				{
 				
+				printf("Enter jersey number:\n");
+				scanf("%d", &num);
+				for(int i = 0; i < PLAY; i++)
+				{
+					if(num == jersey[i])
+					{
+				printf("Enter a new rating for a player:\n");
+				scanf("%d", &rating[i]);
+					}
+				}
+				}
 				break;
 			case 'a':
+				{
 				printf("Enter a rating:\n");
-				scanf("%d",&r);
+				scanf("%d",&num);
+				printf("\nABOVE %d\n", num);
+				for(int i = 0; i< PLAY; i++)
+				{
+					if(num< rating[i])
+					{
+	printf("Player %d -- Jersey number: %d, Rating: %d\n", i+1, jersey[i], rating[i]);
+					}
+				}
+				}
 				break;
 			case 'r':
+				{
 				printf("Enter jersey number:\n");
-				scanf("%d",&j);
+				scanf("%d",&num);
 				printf("Enter a new jersey number:\n");
-				scanf("%d",&j);
-				
-				printf("Enter a rating of new player:\n");
-				scanf("%d",&r);
+				scanf("%d",&new);
+				for(int i = 0; i<PLAY; i++)
+				{
+					if(num == jersey[i])
+					{
+						jersey[i] = new;
+				printf("Enter a rating for the player:\n");
+				scanf("%d",&rating[i]);
+					}
+				}
+		}
 				
 				break;
 			case 'o':
+				{
 				printf("ROSTER\n");
-				for(int i=0;i<5;i++)
+				for(int i=0;i<PLAY;i++)
 				{
 					printf("Player %d -- Jersey number: %d, Rating: %d\n",i+1,jersey[i],rating[i]);
+				}
 				}
 				break;
 case 'q':
 break;
-default:
-printf("You have enter wrong choice\n");
-break;
 						}
 }
-int search(int jersey[], int n)
-{
-	int i;
-	for(i=0;i<5;i++)
-	{
-		if(jersey[i] == n)
-			break;
-	}
-	return i;
-}
-    
-void display(int jersey[], int rating[], int n)
-{
-	for(int i = 0; i<5;i++)
-	{
-		if(rating[i]>= n)
-		{
-			printf("Player %d -- Jersey number: %d, Rating: %d\n",i+1,jersey[i],rating[i]);
-		}
-	}
-}
 
-    return 0;
 }
 // Function Definitions
 
