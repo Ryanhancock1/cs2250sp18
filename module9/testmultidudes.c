572b20ff12adb82302dccf7afac00b36f238c8f2
Ryan Hancock <ryanhancock@mail.weber.edu>
2018-03-20 10:04:22 -0600

    init commit

---
+++ module9/testmultidudes.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testmultidudes.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/20/2018 09:37:51 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ryan Hancock (), ryanhancock@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include "dude.h"
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    SuperDude s1;
+    InitInfoStruct(&s1);
+    ShowInfoStruct(&s1);
+
+    return 0;
+}
+// Function Definitions
+
+
